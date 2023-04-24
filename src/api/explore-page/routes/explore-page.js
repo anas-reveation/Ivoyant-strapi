a0cbe08571f88da6f515e51e48fa8969f0656b45
Koshal Garg <koshal.garg@reveation.io>
2023-04-24 15:32:27 +0530

    life section query added

---
+++ src/api/explore-page/routes/explore-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * explore-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::explore-page.explore-page');
---
+++ src/api/life-section/routes/life-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * life-section router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::life-section.life-section');
